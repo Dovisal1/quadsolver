69c36aadfdc2ead925493777f385eb1756d74e1a
Dovi Salomon <Dovisal1@gmail.com>
2014-11-23 00:17:43 -0500

    removed an unnecessary statement

--- QuadSolver.cpp
+++ QuadSolver.cpp
@@ -53,9 +53,7 @@ int main()
                 cout << "The roots are " << root1 << " and " << root2 << endl;
         else if (!isnan(root1) && !isnan(root2))
             cout << "Only one root: " << root1 << endl;
-	else if ((!isnan(root1)&&isnan(root2))||(isnan(root1)&&!isnan(root2)))
-	    cout << "One imaginary root, and root is " << root1; 
-        else
+	else
             cout << "Imaginary Roots!" << endl;
         
         cout << endl;

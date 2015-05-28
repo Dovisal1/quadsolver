e6d487f1d4ee402deacd92a4267a3053ef432148
Dovisal1 <Dovisal1@gmail.com>
2015-05-28 19:15:28 -0400

    Update QuadSolver.cpp

--- QuadSolver.cpp
+++ QuadSolver.cpp
@@ -2,68 +2,34 @@
 #include <cstdlib>
 #include <cmath>
 
-using namespace std;
-
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
 
     if(argc != 4)
     {
-        cerr << "Usage: " << argv[0] << " <a> <b> <c>\n";
+        std::cerr << "Usage: " << argv[0] << " <a> <b> <c>\n";
         exit(1);
     }
 
     double a = atof(argv[1]);
     double b = atof(argv[2]);
     double c = atof(argv[3]);
-    double root1, root2;
-
+   
     if(!a)
-    {
-	    cerr << argv[0] << ": invalid arguments\n";
-	    print_usage();
-    }
+	    std::cerr << argv[0] << ": invalid arguments\n";
 
-    root1 = (-b + sqrt(pow(b,2) - 4 * a * c) ) / (2 * a);
+    double discriminant = b*b - 4*a*c;
+    double root1 = (-b + sqrt(discriminant) ) / (2 * a);
+    double root2 = (-b - sqrt(discriminant) ) / (2 * a);
 
-    root2 = (-b - sqrt(pow(b,2) - 4 * a * c) ) / (2 * a);
-
-    if (root1 != root2 && !isnan(root1) && !isnan(root2))
-        cout << "The roots are " << root1 << " and " << root2 << endl;
-
-    else if (!isnan(root1) && !isnan(root2))
-        cout << "Only one root: " << root1 << endl;
+    if (discriminant < 0)
+    	std::cout << "Imaginary Roots!" << endl;
+    	
+    else if(discriminant > 0)
+        std::cout << "The roots are " << root1 << " and " << root2 << endl;
 
     else
-        cout << "Imaginary Roots!" << endl;
-
-
-    if (a == 1 && root1 != root2 && !isnan(root1) && !isnan(root2) && int(root1)==root1 && int(root2)==root2)
-    {
-        
-        cout << "Warning: the following feature is in beta!" << endl;
-
-        
-        cout << "Eqaution can be factored into (x";
-        
-        if (b>0)
-        {
-            if (c>0)
-                cout << "+" << root1 * -1 << ")(x+" << root2 * -1 << ")" <<endl;
-            else if (root1>root2)
-                cout << "+" << root2 * -1 << ")(x-" << root1 << ")" << endl;
-            else
-                cout << "+" << root1 * -1 << ")(x-" << root2 << ")" << endl;
-        }
-        else if (c>0)
-            cout << "-" << root1 << ")(x-" << root2 << ")" << endl;
-        else if (root1 > root2)
-            cout << "-" << root1 << ")(x+" << root2 * -1 << ")" << endl;
-        else
-            cout << "-" << root2 << ")(x" << root1 * -1 << ")" << endl;
-
-    }
-        
-
+        std::cout << "Only one root: " << root1 << endl;
+  
     return 0;
 }

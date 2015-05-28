db36826b9d16269f06e527ee24d3d49a46ce4199
Dovisal1 <Dovisal1@gmail.com>
2015-05-28 19:24:04 -0400

    Update QuadSolver.cpp

--- QuadSolver.cpp
+++ QuadSolver.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     if(argc != 4)
     {
         std::cerr << "Usage: " << argv[0] << " <a> <b> <c>\n";
-        exit(1);
+        return 1;
     }
 
     double a = atof(argv[1]);
@@ -16,20 +16,23 @@ int main(int argc, char **argv)
     double c = atof(argv[3]);
    
     if(!a)
-	    std::cerr << argv[0] << ": invalid arguments\n";
+    {
+    	std::cerr << argv[0] << ": invalid arguments\n";
+    	return 2;
+    }
 
     double discriminant = b*b - 4*a*c;
     double root1 = (-b + sqrt(discriminant) ) / (2 * a);
     double root2 = (-b - sqrt(discriminant) ) / (2 * a);
 
     if (discriminant < 0)
-    	std::cout << "Imaginary Roots!" << endl;
+    	std::cout << "Imaginary Roots!\n";
     	
     else if(discriminant > 0)
-        std::cout << "The roots are " << root1 << " and " << root2 << endl;
+        std::cout << "The roots are " << root1 << " and " << root2 << '\n';
 
     else
-        std::cout << "Only one root: " << root1 << endl;
+        std::cout << "Only one root: " << root1 << '\n';
   
     return 0;
 }

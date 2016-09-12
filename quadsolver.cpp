c87c31a5b0c1b41386f4f779fd327aeff8cebba8
Dovi Salomon <Dovisal1@gmail.com>
2016-09-11 23:22:32 -0400

    Made the code more 'c++' like

--- quadsolver.cpp
+++ quadsolver.cpp
@@ -1,32 +1,37 @@
+
 #include <iostream>
-#include <cstdlib>
+#include <string>
 #include <cmath>
+
+#define ERRARG 1
+#define ERRNUM 2
 
 int main(int argc, char *argv[])
 {
+	argv++;
+	argc--;
 
-	if(argc != 4) {
-		std::cerr << "Usage: " << argv[0] << " <a> <b> <c>\n";
-		return 1;
+	if (argc != 3) {
+		std::cerr << "Usage: quad <a> <b> <c>" << std::endl;
+		return ERRARG;
 	}
 
-	double a = atof(argv[1]);
-	double b = atof(argv[2]);
-	double c = atof(argv[3]);
+	double a, b, c;
 
-	if(!a) {
-		std::cerr << argv[0] << ": invalid arguments\n";
-		return 2;
+	try {
+		a = std::stod(argv[0]);
+		b = std::stod(argv[1]);
+		c = std::stod(argv[2]);
+	}
+	catch (const std::invalid_argument& e) {
+		std::cerr << "quad: provide correct coefficients" << std::endl;
+		return ERRNUM;
 	}
 
-	double discriminant = pow(b,2) - 4*a*c;
+	double discriminant = pow(b,2) - (4 * a * c);
 
 
-	if(discriminant == 0) {
-		double root = -b / (2 * a);
-		std::cout << "One root: " << root << '\n';
-	}
-	else if(discriminant > 0) {
+	if (discriminant > 0) {
 		double root1 = (-b + sqrt(discriminant) ) / (2 * a);
 		double root2 = (-b - sqrt(discriminant) ) / (2 * a);
 		std::cout << "Two roots: " << root1 << " and " << root2 << '\n';
@@ -51,6 +56,10 @@ int main(int argc, char *argv[])
 				<< imag2 << "i - " << real;
 		}
 		std::cout << '\n';
+	}
+	else {
+		double root = -b / (2 * a);
+		std::cout << "One root: " << root << '\n';
 	}
 
 	return 0;
